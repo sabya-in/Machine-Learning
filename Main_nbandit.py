da27d80825f5ef68184f84b62b9c6c346513fc80
sabyasacchiM <34243961+sabyasacchiM@users.noreply.github.com>
2021-02-13 20:44:31 +0530

    Update Main_nbandit.py

--- Main_nbandit.py
+++ Main_nbandit.py
@@ -1,5 +1,5 @@
-from Configure import setup
-from Analytics import analysis
+from Configure_nbandit import setup
+from Analytics_nbandit import analysis
 
 def get_q_max(Q_val,epsilon=0):
     from numpy.random import choice
