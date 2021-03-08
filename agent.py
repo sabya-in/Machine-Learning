c5f9b942c3b5ec4bdba73295a5a522b9ef4937d1
sabyasacchiM <34243961+sabyasacchiM@users.noreply.github.com>
2021-03-08 13:09:20 +0530

    Create agent.py
    Agent for control and input of deep Q network - This code does not work, the algorithm implementation for reinforcement learning works but learning with *partial_fit* in Sklearn does not, leading to no improvement in the model.
    
    *Next task - need to investigate and improve; sklearn partial_fit is not working but is much faster than tensorflow's partial_fit*

---
+++ agent.py
@@ -0,0 +1,41 @@
+from neural_net import q_neural_network as dqn
+import numpy as np
+import time as t
+import math
+import gym
+
+env = gym.make('CartPole-v1')
+state_set_size = env.observation_space.shape[0]
+action_set_size = env.action_space.n
+penalty = 4.0
+penalty = float(input("Enter penalty for losing float(default 4.0):") or penalty)
+discount = 0.8
+discount = float(input("Enter reward discount float(default 0.8):") or discount)
+exploration = 0.3
+exploration = float(input("Enter exploration float(default 0.3):") or exploration)
+done = False
+batch_size = 800
+exploration = int(input("Enter Batch Size for neural network training after each game int(default 800):") or batch_size)
+direction = 'none'
+q = dqn(state_set_size,action_set_size,exploration)
+for episodes in range(10000):
+    state = env.reset()
+    state = np.reshape(state, [1, state_set_size])
+    for time in range(300):
+        env.render()
+        action = q.action_decide(state,exploration,math.sqrt(episodes+1))
+        #direction = 'left' if bool(action) else 'right'
+        #print(direction)
+        #t.sleep(0.1)
+        state_n,reward,done,debug = env.step(action)
+        reward -= 0 if not done else penalty
+        q.store(state,action,reward,state_n,done)
+        state = state_n
+        if done:
+            #print('!!!!!!!!!!!!!!!FAILED!!!!!!!!!!!!!!!!! @ '+str(time))
+            q.avg_reward_solver(time)
+            break
+        if len(q.memory) > batch_size:
+            q.train_batch(discount,batch_size)
+            q.flush()
+            print('Average reward in last 100 or less iterations - '+str(q.check_avg_reward_of_solver()))
